35cdb3b99716a2e860305152a0d6f16ba5050047
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-07-22 18:16:25 +0300

    Completed the lab

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
 # lib/config.py
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
-CURSOR = CONN.cursor()
+CONN = sqlite3.connect('company.db') # CONN -A onstant equal to a hash that contains a connetion to a database
+CURSOR = CONN.cursor()  # A constant that allows us to interact with the database and  execute SQL statements
